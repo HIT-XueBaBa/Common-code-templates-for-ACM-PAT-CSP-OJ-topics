c3c2ee494611692e36e714a0d7a667f2e87e26f5
日沉云起 <csjiangfeng@gmail.com>
2019-08-01 15:34:20 +0800

    Update 将数n质因子分解.cpp

--- code/数学/素数/将数n质因子分解.cpp
+++ code/数学/素数/将数n质因子分解.cpp
@@ -1,4 +1,4 @@
-map<int,int>factor;//质因子在factor中，键表示质因子，值表示该质因子个数，注意map会按键排序
+unordered_map<int,int>factor;//质因子在factor中，键表示质因子，值表示该质因子个数，注意map会按键排序
 void getFactor(int n,int arg){//arg为1时表示数n在分子上，作乘法；arg为-1表示数n在分母上，作除法
     if(n==1){
         factor[1]=1;
