808e5b2183a1b746acfc7266d9b791b5e18465b3
日沉云起 <csjiangfeng@gmail.com>
2019-08-06 23:04:38 +0800

    Update 非暴力方法将数n质因子分解.cpp

--- code/数学/素数/非暴力方法/非暴力方法将数n质因子分解.cpp
+++ code/数学/素数/非暴力方法/非暴力方法将数n质因子分解.cpp
@@ -1,9 +1,5 @@
 unordered_map<int,int>factor;//质因子在factor中，键表示质因子，值表示该质因子个数，注意map会按键排序
 void getFactor(int n,int arg){//arg为1时表示数n在分子上，作乘法；arg为-1表示数n在分母上，作除法
-    if(n==1){
-        factor[1]=1;
-        return;
-    }
     findPrime(n+1);//打印n+1以内的素数表存储到prime中
     for(int i:prime){
         while(n%i==0){
