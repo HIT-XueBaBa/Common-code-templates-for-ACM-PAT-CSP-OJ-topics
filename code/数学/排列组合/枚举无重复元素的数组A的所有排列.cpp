70f9b5b0397bfc5c6f1aa4246eacae31f9f33442
日沉云起 <csjiangfeng@gmail.com>
2019-10-10 20:44:30 +0800

    Update and rename 枚举1~n所有排列.cpp to 枚举无重复元素的数组A的所有排列.cpp

---
+++ code/数学/排列组合/枚举无重复元素的数组A的所有排列.cpp
@@ -0,0 +1,11 @@
+void f(vector<int>&A, int index) {
+    if(index==A.size()){
+        //处理当前的排列
+        return;
+    }
+    for(int i=index;i<A.size();++i){
+        swap(A[index],A[i]);
+        f(A,index+1,ans);
+        swap(A[index],A[i]);
+    }
+}
