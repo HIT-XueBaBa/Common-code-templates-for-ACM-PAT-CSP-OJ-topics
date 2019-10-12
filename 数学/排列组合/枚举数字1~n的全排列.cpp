bfc3ed9440f7a4e0d91ffaec3087f146b643ca39
richenyunqi <csjiangfeng@gmail.com>
2019-10-12 11:22:12 +0800

    增加枚举数字1~n的全排列

---
+++ 数学/排列组合/枚举数字1~n的全排列.cpp
@@ -0,0 +1,14 @@
+void f(vector<int>& A, int n) {
+    if (A.size() == n) {  //递归边界
+        for (int i = 0; i < A.size(); i++) printf("%d ", A[i]);
+        printf("\n");
+        return;
+    }
+    for (int i = 1; i <= n; i++) {  //尝试在A末尾填各种整数i
+        if (find(A.begin(), A.end(), i) == A.end()) {
+            A.push_back(i);
+            f(A, n);  //递归调用
+            A.pop_back();
+        }
+    }
+}
