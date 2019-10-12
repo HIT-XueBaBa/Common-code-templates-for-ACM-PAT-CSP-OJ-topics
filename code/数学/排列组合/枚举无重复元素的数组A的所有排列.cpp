9d88c5e0b5dbe1b0a49af4179a9465fd3814a8f1
日沉云起 <csjiangfeng@gmail.com>
2019-10-12 08:48:45 +0800

    Update 枚举无重复元素的数组A的所有排列.cpp

--- code/数学/排列组合/枚举无重复元素的数组A的所有排列.cpp
+++ code/数学/排列组合/枚举无重复元素的数组A的所有排列.cpp
@@ -5,7 +5,7 @@ void f(vector<int>&A, int index) {
     }
     for(int i=index;i<A.size();++i){
         swap(A[index],A[i]);
-        f(A,index+1,ans);
+        f(A,index+1);
         swap(A[index],A[i]);
     }
 }
