f8db6edd399a1413cc3ff68798c1c7c63e09975c
richenyunqi <csjiangfeng@gmail.com>
2019-10-12 10:29:44 +0800

    大整数更新

--- 数学/大整数/大整数取模.cpp
+++ 数学/大整数/大整数取模.cpp
@@ -1,7 +1,7 @@
 //输出n%m的值，其中n<=10^10,m<=10^9
-int bigIntegerMod(const string&n,int m){
-    int ans=0;
-    for(int i=0;i<n.size();++i)
-        ans=(int)((ans*10ll+n[i]-'0')%m);
+long long bigIntegerMod(const string& n, long long m) {
+    long long ans = 0;
+    for (int i = 0; i < n.size(); ++i)
+        ans = ((ans * 10 + n[i] - '0') % m);
     return ans;
 }
--- 数学/大整数/大整数幂取模.cpp
+++ 数学/大整数/大整数幂取模.cpp
@@ -1,10 +1,7 @@
 //求a^n%m
-int powMod(int a,int n,int m){
-    if(n==0)
+long long powMod(long long a, long long n, long long m) {
+    if (n == 0)
         return 1;
-    int x=powMod(a,n/2,m);
-    long long ans=x*1ll*x%m;
-    if(n%2==1)
-        ans=ans*a%m;
-    return (int)ans;
+    long long x = a * a % m;
+    return n % 2 == 0 ? powMod(x, n / 2, m) % m : a * powMod(x, n / 2, m) % m;
 }
