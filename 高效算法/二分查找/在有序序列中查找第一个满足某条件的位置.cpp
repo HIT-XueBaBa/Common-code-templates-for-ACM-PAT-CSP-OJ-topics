4a262d68a7e949a83b08bf942a344703dbd14703
日沉云起 <csjiangfeng@gmail.com>
2019-11-04 21:25:05 +0800

    Update 在有序序列中查找第一个满足某条件的位置.cpp

--- 高效算法/二分查找/在有序序列中查找第一个满足某条件的位置.cpp
+++ 高效算法/二分查找/在有序序列中查找第一个满足某条件的位置.cpp
@@ -1,10 +1,9 @@
 //在有序序列中查找第一个满足某条件的位置，二分区间为左闭右闭的区间[left,right]，f为要满足的条件
 int solve(vector<int>& v, int left, int right, function<bool(int)> f) {
-    int mid;
     while (left <= right) {
         if (left == right)
             return f(left) ? left : left + 1;
-        mid = left + (right - left) / 2;
+        int mid = left + (right - left) / 2;
         if (f(mid))
             right = mid;
         else
