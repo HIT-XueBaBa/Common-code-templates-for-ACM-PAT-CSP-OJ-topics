5405df2656610cb11d88bd644be827d74c9283ac
日沉云起 <csjiangfeng@gmail.com>
2019-11-04 21:24:46 +0800

    Update 在有序序列中查找某数value的位置.cpp

--- 高效算法/二分查找/在有序序列中查找某数value的位置.cpp
+++ 高效算法/二分查找/在有序序列中查找某数value的位置.cpp
@@ -1,8 +1,7 @@
 //在有序序列中查找某数value的位置，二分区间为左闭右闭的区间[left,right]
 int binarySearch(vector<int>& v, int left, int right, int value) {
-    int mid;
     while (left <= right) {
-        mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
         if (v[mid] == value)
             return mid;
         else if (v[mid] < value)
