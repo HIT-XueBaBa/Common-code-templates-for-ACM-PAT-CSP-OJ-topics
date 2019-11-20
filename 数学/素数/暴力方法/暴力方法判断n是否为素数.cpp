7e632e6b843d255c25992cb2a064563b2bc16f06
richenyunqi <csjiangfeng@gmail.com>
2019-11-20 23:42:21 +0800

    更新素数

--- 数学/素数/暴力方法/暴力方法判断n是否为素数.cpp
+++ 数学/素数/暴力方法/暴力方法判断n是否为素数.cpp
@@ -1,8 +1,8 @@
 bool isPrime(LL n) {
-    if (n < 2)
+    if (n < 2)  //n小于2，一定不是素数
         return false;
-    for (LL i = 2; i <= (LL)sqrt(n * 1.0); ++i)
-        if (n % i == 0)
+    for (LL i = 2; i <= (LL)sqrt(n * 1.0); ++i)  //遍历2~根号n所有的数
+        if (n % i == 0)  //n能被i整除，说明n不是素数
             return false;
-    return true;
+    return true;  //n不能被2~根号n任何数整除，则n是素数
 }
---
+++ 数学/素数/非暴力方法/埃氏筛法求解n以内的素数表.cpp
@@ -0,0 +1,12 @@
+//以下代码求解[2,n]以内的素数表
+vector<LL> prime;  //素数表存储在prime中，prime是全局变量
+void Eratosthenes(LL n) {
+    //f[i]为true，表示数字i被筛去了；如果f[i]为false，表示数字i没有被筛去
+    bitset<100005> f;  //这里默认n小于100005
+    for (LL i = 2; i <= n; ++i)
+        if (!f[i]) {  //i没有被筛去
+            prime.push_back(i);
+            for (LL j = i + i; j <= n; j += i)  //筛去i的所有倍数
+                f[j] = true;
+        }
+}
