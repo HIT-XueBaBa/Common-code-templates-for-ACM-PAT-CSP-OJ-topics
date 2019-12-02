a7987d14ec7d8b969daf828ce1288964ef7e4f10
richenyunqi <csjiangfeng@gmail.com>
2019-12-02 17:28:57 +0800

    修正前缀树

--- 实用数据结构/字符串/前缀树.cpp
+++ 实用数据结构/字符串/前缀树.cpp
@@ -3,6 +3,7 @@ struct TrieNode {
     array<TrieNode*, 26> children{};  //数组长为26表示单词只由26个小写英文字母组成，可按实际需要修改
 };
 TrieNode* root = new TrieNode();
+//插入一个单词
 void insert(TrieNode* r, const string& word, int p = 0) {
     auto& i = r->children[word[p] - 'a'];
     if (i == nullptr)
@@ -11,4 +12,13 @@ void insert(TrieNode* r, const string& word, int p = 0) {
         ++i->count;
     else
         insert(i, word, p + 1);
+}
+//查找一个单词
+bool searchWord(TrieNode* r, const string& word, int p = 0) {
+    auto& i = r->children[word[p] - 'a'];
+    if (i == nullptr)
+        return false;
+    if (p == word.size() - 1)
+        return i->count > 0;  //如果是查找前缀把这条语句换成return true;就可以了
+    return searchWord(i, word, p + 1);
 }
