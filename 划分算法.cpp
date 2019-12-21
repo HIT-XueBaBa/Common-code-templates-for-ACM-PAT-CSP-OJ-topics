74befb5e599734ab88b0cdd686858440fca147ab
richenyunqi <csjiangfeng@gmail.com>
2019-12-21 21:43:04 +0800

    更新划分算法

--- 划分算法.cpp
+++ 划分算法.cpp
@@ -1,6 +1,7 @@
-//划分算法，将数组nums分成<t、==t、>t三部分
-void partition(vector<LL>& nums, LL t) {
-    for (LL i = 0, j = 0, k = nums.size() - 1; i <= k;) {
+//划分算法，将数组nums分成<t、==t、>t三部分，返回一个2维array，表示==t的部分的首元素索引、尾元素的下一个元素的索引
+array<LL, 2> partition(vector<LL>& nums, LL t) {
+    LL i = 0, j = 0, k = nums.size() - 1;
+    while (i <= k) {
         if (nums[i] > t)
             swap(nums[i], nums[k--]);
         else if (nums[i] < t)
@@ -8,4 +9,5 @@ void partition(vector<LL>& nums, LL t) {
         else
             ++i;
     }
+    return {j, i};
 }
